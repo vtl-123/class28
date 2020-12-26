af7cfe3e16d3a1c84dc493f2bcf1ff64cdb9eee1
vtl-123 <75170543+vtl-123@users.noreply.github.com>
2020-12-26 19:51:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class28
+# angryBirdsStage2
+Angry Birds stage 2 with Class Inheritance and Images
---
+++ slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+constructor(bodyA,pointB){
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.07,
+        length:15
+    }
+    this.pointB=pointB
+    this.sling=Constraint.create(options);
+    World.add(world,this.sling)
+}
+fly(){
+    this.sling.bodyA=null;
+}
+display(){
+    if(this.sling.bodyA){
+        var pointA=this.sling.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);  
+        }   
+    }
+}       
